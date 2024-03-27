d96519046935d5db2e2a6a6682320211ebad1c83
mooaz sayyed <sayyedmooaz@gmail.com>
2024-03-27 23:01:51 +0530

    commit packages

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 import express from "express";
-import colors from "colors";
+// import colors from "colors";
 import dotenv from "dotenv";
-import morgan from "morgan";
-import { connectDB } from "./config/db.js";
+// import morgan from "morgan";
+// import { connectDB } from "./config/db.js";
 // import authRoutes from "./routes/authRoute.js";
 
 //configure env
 dotenv.config();
 
 //databse config
-connectDB();
+// connectDB();
 
 //rest object
 const app = express();
 
 //middelwares
 app.use(express.json());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 //routes
 // app.use("/api/v1/auth", authRoutes);
 
 //rest api
-// app.get("/", (req, res) => {
-//   res.send("<h1>Welcome to ecommerce app</h1>");
-// });
+app.get("/", (req, res) => {
+   res.send("<h1>Welcome to ecommerce app</h1>");
+});
 
 //PORT
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8005;
 
 //run listen
 app.listen(PORT, () => {
