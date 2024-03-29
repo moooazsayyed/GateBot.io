859cfbb67f815649e4039cd79538da7714b7a53a
strange051 <agrawalshashwat22@gmail.com>
2024-03-29 11:53:58 +0530

    edited navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,29 +1,20 @@
 import React from 'react';
+import './navbar.css';
 
 export default function Navbar() {
   return (
     <div>
       <div className="navbar">
 
-        <div className="left">
-          <div className="flat">
-            <p>A-101</p>
-          </div>
+        <div className="flat">
+          <p>A-101</p>
         </div>
-        
-        <div className="right">
-          <div className="icon">
-            <a href="#">🔍</a>
-          </div>
-          <div className="icon">
-            <a href="visitor.html">🔔</a>
-          </div>
-          <div className="icon">
-            <a href="#">💬</a>
-          </div>
-          <div className="icon">
-            <a href="#">👤</a>
-          </div>
+
+        <div className="icons">
+          <a href="#">🔍</a>
+          <a href="visitor.html">🔔</a>
+          <a href="#">💬</a>
+          <a href="#">👤</a>
         </div>
 
       </div>
